a65295c66e5faadc2669adbeabb09d57883c314e
Muhamad Ikhsan Laisa <m.ikhsan.staff@dansmultipro.com>
2020-03-11 13:55:16 +0700

    update home slider

--- app/src/main/java/com/example/girlsthings/adapter/ViewPagerAdapter.java
+++ app/src/main/java/com/example/girlsthings/adapter/ViewPagerAdapter.java
@@ -1,18 +1,68 @@
 package com.example.girlsthings.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.girlsthings.R;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 public class ViewPagerAdapter extends PagerAdapter {
+
+    Context context;
+    List<String> slider;
+    LayoutInflater layoutInflater;
+
+    public ViewPagerAdapter(Context context, List<String> slider) {
+        this.context = context;
+        this.slider = slider;
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return slider.size();
     }
 
     @Override
     public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-        return false;
+        return view == object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        View view = layoutInflater.inflate(R.layout.viewpager_image, null);
+        final ProgressBar progressBar = view.findViewById(R.id.viewPagerProgressBar);
+        final ImageView image = view.findViewById(R.id.viewPagerImage);
+        final ImageView imageBackground = view.findViewById(R.id.viewPagerBackground);
+
+        container.addView(view);
+
+        try {
+            Picasso.get().load(slider.get(position).trim())
+                    .error(R.drawable.upload)
+                    .into(image);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        ViewPager viewPager = (ViewPager) container;
+        View view = (View) object;
+        viewPager.removeView(view);
     }
 }
--- app/src/main/java/com/example/girlsthings/fragment/home/HomeFragment.java
+++ app/src/main/java/com/example/girlsthings/fragment/home/HomeFragment.java
@@ -1,32 +1,39 @@
 package com.example.girlsthings.fragment.home;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import com.example.girlsthings.R;
 import com.example.girlsthings.adapter.ViewPagerAdapter;
-import com.potyvideo.slider.library.SliderLayout;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
 
-    ViewPagerAdapter viewPagerAdapter;
-    ViewPager viewPager;
+    private ViewPagerAdapter viewPagerAdapter;
+    private ViewPager viewPager;
+    private List<String> slider = new ArrayList<>();
+    private TabLayout tabLayout;
+    private Activity activity;
 
     public HomeFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -43,6 +50,31 @@ public class HomeFragment extends Fragment {
     }
 
     private void showSlider() {
+        slider.add("https://cdn2.tstatic.net/kaltim/foto/bank/images/promo-guardian.jpg");
+        slider.add("https://cf.shopee.co.id/file/55250dc317f63f9b4f6f96915b2980d3");
 
+        viewPagerAdapter = new ViewPagerAdapter(getActivity(), slider);
+        viewPager = getActivity().findViewById(R.id.slider);
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout = getActivity().findViewById(R.id.viewPagerIndicator);
+        tabLayout.setupWithViewPager(viewPager, true);
+        viewPager.setCurrentItem(slider.size() - 1);
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (viewPager.getCurrentItem() < slider.size() - 1) {
+                            viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
+                        } else {
+                            viewPager.setCurrentItem(0);
+                        }
+                    }
+                });
+            }
+        }, 4000, 6000);
     }
 }
